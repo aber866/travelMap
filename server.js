6ac44e2387c45d036a85a46b493689a96aaaaa7b
Valverde-PC\Valverde <aber866@gmail.com>
2017-05-17 19:49:59 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const path = require('path'),
+	express = require('express');
+	app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, function () {
+  	console.log('Example app listening on port 3000!');
+});
