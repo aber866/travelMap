a3b4c04d83b87509a878ac0fd32afa2a11787201
Valverde-PC\Valverde <aber866@gmail.com>
2017-05-17 20:43:54 +0100

    heroku errors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path'),
 	app = express(),
 	cool = require('cool-ascii-faces');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
@@ -13,6 +15,6 @@ app.get('/cool', function(request, response) {
   	response.send(cool());
 });
 
-app.listen(3000, function () {
-  	console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), function() {
+  	console.log('Node app is running on port', app.get('port'));
 });
