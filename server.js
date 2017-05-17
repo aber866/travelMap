1a95d98fd3d2b21de3bf47ca9786ae10f0858458
Valverde-PC\Valverde <aber866@gmail.com>
2017-05-17 20:24:13 +0100

    demo

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const path = require('path'),
 	express = require('express');
-	app = express();
+	app = express(),
+	cool = require('cool-ascii-faces');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(request, response) {
+  	response.render('public/index')
+});
+
+app.get('/cool', function(request, response) {
+  	response.send(cool());
+});
 
 app.listen(3000, function () {
   	console.log('Example app listening on port 3000!');
