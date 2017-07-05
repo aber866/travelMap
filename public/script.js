159e9046ee1b810f08dc66413afc3149a6744d39
Alberto Valverde <alberto.valverde@contractor.isbanuk.com>
2017-07-05 17:31:03 +0100

    Photos added

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 var map = AmCharts.makeChart( "chartdiv", {
     "type": "map",
     "theme": "light",
+    projection: "miller",
     "dataProvider": {
         "map": "worldHigh",
         "zoomLevel": 5,
@@ -24,7 +25,108 @@ var map = AmCharts.makeChart( "chartdiv", {
         }, {
             "id": "MA",
             "color": "#FF8000"
+        }],
+        "images": [ {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: -8,
+            latitude: 39.5,
+            title: "title",
+            value: 2,
+            label: 2,
+            balloonText: "<img src='img/oporto.jpg' style='width: 100px; float: left; margin-right: 10px;'>" +
+                "<span>" +
+                "<strong>" + "Portugal: 2 viajes, 4 ciudades" + ":</strong></br> Algarve (Laos, San Vicente, Villa d'Obispo): Agosto 2014 </br> Oporto: Julio 2016" +
+                "<img src='img/algarve.jpg' style='width: 150px; float: right; margin-left: 10px; margin-top:10px'>",
+            zoomLevel: 10,
+            labelShiftY: -2,
+        },
+        {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: -5,
+            latitude: 32,
+            title: "title",
+            value: 1,
+            label: 1,
+            balloonText: "<img src='img/marruecos.jpg' style='width: 150px; float: left; margin-right: 10px;'>" +
+                "<strong>" + "Marruecos: 1 viaje, 3 ciudades" + ":</strong></br> Marruecos (Marrakech, Casablanca, Essaouira): Agosto 2015",
+            zoomLevel: 10,
+            labelShiftY: -2,
+        },
+        {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: 2,
+            latitude: 46,
+            title: "title",
+            value: 1,
+            label: 1,
+            balloonText: "<img src='img/paris.jpg' style='width: 150px; float: left; margin-right: 10px;'>" +
+                "<strong>" + "Francia: 1 viaje, 1 ciudad" + ":</strong></br> Paris: Abril 2017",
+            zoomLevel: 10,
+            labelShiftY: -2,
+        },
+        {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: 10,
+            latitude: 56,
+            title: "title",
+            value: 1,
+            label: 1,
+            balloonText: "<img src='img/copenaghe.jpg' style='width: 200px; float: right; margin-left: 10px;'>" +
+                "<strong>" + "Dinamarca: 1 viaje, 1 ciudad" + ":</strong></br> Copenhague: Mayo 2017",
+            zoomLevel: 10,
+            labelShiftY: -2,
+        }, {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: -4,
+            latitude: 40,
+            title: "title",
+            value: 3,
+            label: 3,
+            balloonText: "<img src='img/barcelona.jpg' style='width: 100px; float: left; margin-right: 10px;'>" +
+                "<span>" +
+                "<strong>" + "Espania: muchos viajes" + ":</strong></br> Barcelona: Diciembre 2016 </br> Huesca: Diciembre 2015" +
+                "<img src='img/huesca.jpg' style='width: 150px; float: right; margin-left: 10px; margin-top:10px'>",
+            zoomLevel: 10,
+            labelShiftY: -2
+        },
+        {
+            type: "circle",
+            width: 20,
+            height: 20,
+            longitude: -2,
+            latitude: 54,
+            title: "title",
+            value: 1,
+            label: 1,
+            balloonText: "proximamente...",
+            zoomLevel: 10,
+            labelShiftY: -2,
         }]
+    },
+    balloon: {
+        maxWidth: 302,
+        adjustBorderColor: true,
+        borderThickness: 1,
+        color: "#444444",
+        fontSize: 12,
+        textAlign: "left",
+        fixedPosition: false
+    },
+    imagesSettings: {
+        labelPosition: "middle",
+        labelColor: "#fff",
+        labelRollOverColor: "#fff",
+        color: "#EC0000"
     },
     "areasSettings": {
         "rollOverOutlineColor": "#111111",
